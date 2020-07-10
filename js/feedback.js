dd9ea9f8f9ea4d5bbe46f97f8248914101a68344
Darya Marchik <marchik2764@gmail.com>
2020-07-10 11:29:22 +0300

    Форма обратной связи (модальное окно)

---
+++ js/feedback.js
@@ -0,0 +1,59 @@
+var feedbackLink = document.querySelector(".red-button-feedback");
+var feedbackPopup = document.querySelector(".modal-feedback");
+var feedbackClose = feedbackPopup.querySelector(".modal-close");
+var feedbackForm = feedbackPopup.querySelector(".feedback-form");
+var feedbackName = feedbackPopup.querySelector(".feedback-name-user");
+var feedbackEmail = feedbackPopup.querySelector(".feedback-email-user");
+var feedbackLetter = feedbackPopup.querySelector(".feedback-letter");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("name"),
+  storage = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+feedbackLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedbackPopup.classList.add("modal-show");
+  if (storage) {
+  feedbackName.value = storage
+  feedbackEmail.value = storage;
+  feedbackLetter.focus();
+  } else {
+  feedbackName.focus();
+  }
+});
+
+feedbackClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedbackPopup.classList.remove("modal-show");
+  feedbackPopup.classList.remove("modal-error");
+});
+
+feedbackForm.addEventListener("submit", function (evt) {
+  if (!feedbackName.value || !feedbackEmail.value) {
+    evt.preventDefault();
+    feedbackPopup.classList.remove("modal-error");
+    feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
+    feedbackPopup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+    localStorage.setItem("name", feedbackName.value);
+    localStorage.setItem("email", feedbackEmail.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (feedbackPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      feedbackPopup.classList.remove("modal-show");
+      feedbackPopup.classList.remove("modal-error");
+    }
+  }
+});
